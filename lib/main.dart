ff9e87d853228ce3ea4e671daa7da78944c8ab2d
mohamedalaaibrahim <mohamedalaaabdelhamid@gmail.com>
2022-07-16 14:42:28 +0200

    feat: added firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:b_sure/routes.dart';
 import 'package:b_sure/screens/splash/splash_screen.dart';
 import 'package:b_sure/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -14,11 +22,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        // theme: ThemeData.dark(),
-        theme: theme(),
-        initialRoute: SplashScreen.routeName,
-        routes: routes);
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      // theme: ThemeData.dark(),
+      theme: theme(),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+      // home: Scaffold(
+      //   floatingActionButton: FloatingActionButton(
+      //     onPressed: () => FirebaseFirestore.instance
+      //         .collection('testing')
+      //         .add({'timestamp': Timestamp.fromDate(DateTime.now())}),
+      //     child: const Icon(Icons.add),
+      //   ),
+      //   body: StreamBuilder(
+      //     stream: FirebaseFirestore.instance.collection('testing').snapshots(),
+      //     builder: (
+      //       context,
+      //       AsyncSnapshot<QuerySnapshot> snapshot,
+      //     ) {
+      //       if (!snapshot.hasData) return const SizedBox.shrink();
+      //       return ListView.builder(
+      //         itemCount: snapshot.data?.docs.length,
+      //         itemBuilder: (context, index) {
+      //           final docData = snapshot.data!.docs[index];
+      //           final dateTime =
+      //               (docData.get('timestamp') as Timestamp).toDate();
+      //           return ListTile(
+      //             title: Text(dateTime.toString()),
+      //           );
+      //         },
+      //       );
+      //     },
+      //   ),
+      // ),
+    );
   }
 }
